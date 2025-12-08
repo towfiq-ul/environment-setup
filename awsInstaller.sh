9524214f11db2a0dec11d983f34d5a77945ce846
towfiq-bk <towfiqul.exabyting@bkash.com>
2025-12-08 17:45:31 +0600

    update script file

---
+++ awsInstaller.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+sudo snap install aws-cli --classic
+
+aws --version
--- javaOracleInstall.sh
+++
@@ -1,6 +0,0 @@
-sudo add-apt-repository ppa:linuxuprising/java -y
-sudo apt update
-sudo apt-get install oracle-java17-installer oracle-java17-set-default
-#sudo apt autoremove oracle-java17-installer oracle-java17-set-default --purge
-
-echo "Thank You! JDK17 installed successfully."
