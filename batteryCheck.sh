ce219f0594439bdf4e9b2e7af64e73c54cd1e596
Towfiqul Islam <towfiq.106@gmail.com>
2026-08-19 20:11:46 +0600

    update batteryCheck script

--- batteryCheck.sh
+++ batteryCheck.sh
@@ -1,1 +1,26 @@
-upower -i /org/freedesktop/UPower/devices/battery_BAT0
+#!/bin/bash
+
+HISTORY_DIR="$HOME/.cache/batteryCheck"
+HISTORY_FILE="$HISTORY_DIR/history.log"
+
+mkdir -p "$HISTORY_DIR"
+
+OUTPUT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
+
+echo "$OUTPUT"
+
+{
+  echo "===== $(date '+%Y-%m-%d %H:%M:%S') ====="
+  echo "$OUTPUT"
+  echo
+} >> "$HISTORY_FILE"
+
+# keep only the last 1 year of history
+CUTOFF=$(date -d '1 year ago' '+%Y-%m-%d')
+awk -v cutoff="$CUTOFF" '
+  /^===== [0-9]{4}-[0-9]{2}-[0-9]{2} / {
+    entry_date = substr($2, 1, 10)
+    keep = (entry_date >= cutoff)
+  }
+  keep { print }
+' "$HISTORY_FILE" > "$HISTORY_FILE.tmp" && mv "$HISTORY_FILE.tmp" "$HISTORY_FILE"
